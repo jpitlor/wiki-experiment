65cedebdaf26e5aa8d81730c509a55e48d66c950
Jordan Pitlor <jordan@pitlor.dev>
2025-12-03 19:37:03 -0500

    remove cache

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/jpitlor/Code/CampCourses/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/jpitlor/Code/CampCourses/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jpitlor/Code/CampCourses/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jpitlor/Code/CampCourses/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/jpitlor/Code/CampCourses/src/css/custom.css"),
-];
